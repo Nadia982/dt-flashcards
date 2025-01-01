fcd9798ad32b4a7b51ee55a104f45478cdf54ca7
Natalie Gillam <ngillam@hotmail.co.uk>
2025-01-01 17:03:25 +0000

    first commit

---
+++ js/questions.js
@@ -0,0 +1,180 @@
+const questions = [
+  {
+    id: 0,
+    q: "Which electronic components are in the torch?",
+    definition: "(1) Resistor, (2) Light Emitting Diode (LED), (3) Switch, (4) Battery holder, (5) Printed Circuit Board (PCB)",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 1,
+    q: "What is the circuit diagram symbol for an LED?",
+    definition: "",
+    img: "./images/LED.png",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 2,
+    q: "Give 2 reasons why we use circuit diagram symbols",
+    definition: "(1) They are quicker to draw than the actual component. (2) Universally understood – international symbols",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 3,
+    q: "What is a system?",
+    definition: "A group of things working together to achieve an outcome.",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 4,
+    q: "What are the 3 stages a system can be broken into in a block diagram?",
+    definition: "(1) input, (2) process, (3) outcome.",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 5,
+    q: "Why do manufacturers sometimes want to limit a product's lifespan?",
+    definition: "To increase sales of newer products (and therefore make more money).",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 6,
+    q: "How can a company limit the life of a product (list 4)?",
+    definition: "(1) Use cheaper materials which break easily. (2) Make it difficult to replace components that break. (3) Make replacement components expensive or difficult to get hold of. (4) Stop providing support or updates.",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 7,
+    q: "What does it mean when we say that something is obsolete?",
+    definition: "",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 6,
+    q: "",
+    definition: "",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 6,
+    q: "",
+    definition: "",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  {
+    id: 6,
+    q: "",
+    definition: "",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+  // template 
+  {
+    id: 6,
+    q: "",
+    definition: "",
+    options: ["yes", "no"],
+    a: 0,  
+  },
+
+ ]
+
+//******************Line 2******************* */
+// What is obsolescene?
+// What is planned obsolescence?	
+// What is perceived obsolescence?	
+// What is an electronic input?
+
+//  An electronic input device is something that sends an electronic signal into a device.	
+// Perceived obsolescence is when people believe a product is obsolete. 
+// This can be due to advertising, fashions or trends. 	
+// Planned obsolescence is when a manufacture deliberately designs a product to stop working after a set time, even though it could be designed to work for longer. 	
+// //  
+
+//******************Line 3******************* */
+//  What are examples of electronic inputs?	
+// What is an electronic output?	
+// What are examples of electronic outputs?	
+// What is an electric circuit?	
+// What is an electrical conductor? 
+
+//  An electrical conductor is a material that electricity can flow through. 	
+// -An electronic circuit is made up of electronic components connected together. 
+//  -For a circuit to work, the load (the component that uses energy) and energy source (battery) must form a ‘closed’ loop. 	
+// Lights, speakers and motors are examples of electronic outputs. 	
+// Output devices in electronic systems transform electrical energy into another type of energy, such as light, sound or kinetic energy (movement).	
+// Switches, buttons and microphones are examples of electronic inputs.
+
+//******************Line 4******************* */
+
+//  What is an electrical insulator?	
+// What two things does a battery do in a circuit?	
+// In a circuit, what is a load?	
+// What do resistors do?	
+// How can you tell the value (resistance) of a resistor?
+
+// The coloured bands on a resister indicate its resistance. 
+// They can be used to calculate the value of the resistor. 	
+// Resistors are electronic components that restrict the flow of current around a circuit. 	
+// The component in a circuit that uses energy, such as an LED, buzzer or motor.
+//  A battery forces electrons around a circuit and gives electrons energy, which they can transfer to a load in the circuit. 
+// 	An electrical insulator is a material that electricity cannot flow through.
+
+//******************Line 5 ******************* */
+
+//  What are the units used to measure resistance? 	
+// How do you fix components to a PCB?	
+// What are the safety precautions you should take when soldering (list 4)?	
+// How are polymers produced?	
+//  What is the difference between a renewable and non-renewable material?
+
+//  -Non-renewable material will eventually run out. 
+//  -Renewable materials can be replaced. 	
+// -Crude oil is made up of a mixture of lots of different hydrocarbons. 
+//  -Fractional distillation is used to separate the out.  
+//  -Naphtha is used to produce monomers. 
+//  -Monomers are joined together to produce polymers. 	
+// -Always wear goggles. 
+//  -Use a stand to hold the PCB.
+//  -Only one person uses the soldering iron.
+//  -Put the soldering iron in the stand 
+//  whenever it is not being used.  	Components are fixed to a PCB using solder. 	Ohms (Ω)
+
+//******************Line 6 ******************* */
+//  What are examples of renewable and non-renewable material?	
+// State 4 facts about thermoplastics. 	
+// State 4 facts about thermosetting plastics. 	
+// What is sustainability?	
+// What are the 6 Rs?
+
+//  -Rethink 
+//  -Reuse
+//  -Recycle 
+//  -Repair 
+//  -Reduce 
+//  -Refuse	Sustainability means meeting the needs of the present, without compromising the ability of future generations to meet their own needs. 	Crosslinks form when plastic is shaped for the first time. 
+ 
+//  i. Can only be formed once. 
+//  ii. Can withstand higher 
+//  temperature
+//  iii. Used in electrical and 
+//  cooking items	No crosslinks = can be melted and shaped many times 
+ 
+//  i.  Can be recycled  
+//  ii. Has a lower melting than thermosetting plastics. 
+//  iii. Most plastic items are made using thermoplastics. 	-Non-renewable material include metals and plastics. 
+//  -Renewable materials include timber and cotton.
+  
+
+
+
+ 
